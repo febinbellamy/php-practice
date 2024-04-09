cce5d31cbe122b9cce399254f29b121931e0db96
Febin Bellamy <febinbellamy@gmail.com>
2024-04-08 20:01:23 -0400

    Added FIXME: Get Full Name exercise (Codewars)

---
+++ codewarsPhpChallenges/fixmeGetFullName.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+https://www.codewars.com/kata/597c684822bc9388f600010f/train/php
+
+The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null, but may be empty.
+
+Task
+Fix the bug so we can all go home early.
+*/
+
+class Dinglemouse {
+    public function __construct($firstName="", $lastName="") {
+      $this->firstName = $firstName;
+      $this->lastName = $lastName;
+    }
+    
+    public function getFullName() {
+      $separator = $this->lastName && $this->firstName ? " " : "";
+      return "{$this->firstName}{$separator}{$this->lastName}";
+    }
+  }
+  
