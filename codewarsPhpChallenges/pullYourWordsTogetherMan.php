f80aaaab5a4669be6a2c69d155ebce643aaca9f5
Febin Bellamy <febinbellamy@gmail.com>
2024-04-09 10:31:45 -0400

    Created .gitignore file

--- codewarsPhpChallenges/pullYourWordsTogetherMan.php
+++ codewarsPhpChallenges/pullYourWordsTogetherMan.php
@@ -13,17 +13,18 @@ Every time that it tries to say a sentence, instead of formatting it in normal E
 Robbie has pulled multiple all-nighters to get this project finished, and he needs some beauty sleep. So, he wants you to write the last part of his code, a sentencify function, which takes the output that the machine gives, and formats it into proper English orthography.
 
 Your function should:
+– Capitalise the first letter of the first word.
+– Add a period (.) to the end of the sentence.
+– Join the words into a complete string, with spaces.
+– Do no other manipulation on the words.
 
-Capitalise the first letter of the first word.
-Add a period (.) to the end of the sentence.
-Join the words into a complete string, with spaces.
-Do no other manipulation on the words.
 Here are a few examples of what your function should do:
 
-Input	Output
-["i", "am", "an", "AI"]	"I am an AI."
-["FIELDS","of","CORN","are","to","be","sown"]	"FIELDS of CORN are to be sown."
-["i'm","afraid","I","can't","let","you","do","that"]	"I'm afraid I can't let you do that."
+Input	                                                Output
+["i", "am", "an", "AI"]	                                "I am an AI."
+["FIELDS","of","CORN","are","to","be","sown"]	        "FIELDS of CORN are to be sown."
+["i'm","afraid","I","can't","let","you","do","that"]    "I'm afraid I can't let you do that."
+
 (Any translations (eg: to Java/C#/C++/Typescript) would be greatly appreciated!)
 */
 
