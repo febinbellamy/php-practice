e446aa94e25495540916eab88b70396002f3b650
Febin Bellamy <febinbellamy@gmail.com>
2024-04-26 11:16:44 -0400

    Added Strong Number Special Numbers Series 2 exercise (Codewars)

---
+++ codewarsPhpChallenges/strongNumberSpecialNumbersSeriesTwo.php
@@ -0,0 +1,34 @@
+<?php 
+/* 
+https://www.codewars.com/kata/5a4d303f880385399b000001
+
+Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+
+For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Task
+Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+
+Examples
+- 1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+- 123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+- 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+- 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+*/
+
+function factorial($n) {
+  if ($n === 0) {
+    return 1;
+  }
+  return $n * factorial($n - 1);
+}
+
+function strong($n) {
+  $arrOfDigits = str_split((string)$n);
+  $total = 0;
+  for($i = 0; $i < count($arrOfDigits); $i++) {
+    $currentDigit = (int)$arrOfDigits[$i];
+    $total += factorial($currentDigit);
+  }
+  return $n === $total ? "STRONG!!!!" : "Not Strong !!";
+}
